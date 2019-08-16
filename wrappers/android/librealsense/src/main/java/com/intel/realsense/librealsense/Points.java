fdc58b3879cb60230e6ea3ea2cf2db9fe4fcf8e0
Erik Gallegos <erikgallegos25@gmail.com>
2019-08-16 13:00:57 -0700

    added apis for point data

--- wrappers/android/librealsense/src/main/java/com/intel/realsense/librealsense/Points.java
+++ wrappers/android/librealsense/src/main/java/com/intel/realsense/librealsense/Points.java
@@ -1,8 +1,36 @@
 package com.intel.realsense.librealsense;
 
 public class Points extends Frame {
+
+    private float[] mData;
+    private float[] mTextureCoordinates;
+
     protected Points(long handle) {
         super(handle);
+    }
+
+    public float[] getVertices() {
+        if (mData == null){
+            mData = new float[getCount() * 3];
+            getData(mData);
+        }
+        return mData;
+    }
+
+    public float[] getTextureCoordinates() {
+        if(mTextureCoordinates == null){
+            mTextureCoordinates = new float[getCount() * 2];
+            getTextureCoordinates(mTextureCoordinates);
+        }
+        return mTextureCoordinates;
+    }
+
+    public void getTextureCoordinates(float[] data) {
+        nGetTextureCoordinates(mHandle, data);
+    }
+
+    public void getData(float[] data) {
+        nGetData(mHandle, data);
     }
 
     public int getCount(){
@@ -13,5 +41,7 @@ public class Points extends Frame {
     }
 
     private static native int nGetCount(long handle);
+    private static native void nGetData(long handle, float[] data);
+    private static native void nGetTextureCoordinates(long handle, float[] data);
     private static native void nExportToPly(long handle, String filePath, long textureHandle);
 }
